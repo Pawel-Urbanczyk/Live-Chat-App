e4a77aad4280015145d41a77fbf99eaca909074d
Paweł Urbańczyk <pawel.urbanczyk90@gmail.com>
2018-08-31 15:10:38 +0200

    Wprowadzenie czatu grupowego

--- index.php
+++ index.php
@@ -28,6 +28,10 @@ if(!isset($_SESSION['user_id'])){
 
         <div class="table-responsive">
             <h4 align="center">Online User</h4>
+            <div class="col-md-2 col-sm-3">
+                <input type="hidden" id="is_active_group_chat_window" value="no">
+                <button type="button" name="group_chat" id="group_chat" class="btn btn-warning btn-xs">Group Chat</button>
+            </div>
             <p align="right">Hi -<b> <?php echo $_SESSION['username']; ?></b>-<a href="logout.php">LogOut</a></p>
             <div id="user_details"></div>
             <div id="user_model_details"></div>
@@ -35,6 +39,18 @@ if(!isset($_SESSION['user_id'])){
     </div>
 </body>
 </html>
+
+    <div id="group_chat_dialog" title="Group Chat Window">
+        <div id="group_chat_history" style="height:400px; border: 1px solid #ccc;overflow-y:scroll;margin-bottom:24px;padding:16px;">
+
+        </div>
+        <div class="form-group">
+            <textarea name="group_chat_message" id="group_chat_message" class="form-control"></textarea>
+        </div>
+        <div class="form-group" align="right">
+            <button type="button" name="send_group_chat" id="send_group_chat">Send</button>
+        </div>
+    </div>
 
 <script>
     $(document).ready(function () {
@@ -45,6 +61,7 @@ if(!isset($_SESSION['user_id'])){
             update_last_activity();
             fetch_user();
             update_chat_history_data();
+            fetch_group_chat_history();
         }, 5000);
 
         function fetch_user() {
@@ -169,6 +186,58 @@ if(!isset($_SESSION['user_id'])){
                 }
             });
         });
+
+        $('#group_chat_dialog').dialog({
+            autoOpen:false,
+            width: 400
+        });
+
+        $('#group_chat').click(function(){
+            $('#group_chat_dialog').dialog('open');
+            $('#is_active_group_chat_window').val('yes');
+            fetch_group_chat_history();
+        });
+
+        $('#send_group_chat').click(function(){
+            var chat_message = $('#group_chat_message').val();
+            var action = 'insert_data';
+
+            if(chat_message != ''){
+
+                $.ajax({
+                    url:"group_chat.php",
+                    method:"POST",
+                    data:{chat_message:chat_message, action:action},
+                    success: function(data){
+                        $('#group_chat_message').val('');
+                        $('#group_chat_history').html(data);
+                    }
+                });
+
+            }
+        });
+        
+        function fetch_group_chat_history(){
+
+            var group_chat_dialog_active = $('#is_active_group_chat_window').val();
+            var action = "fetch_data";
+
+            if(group_chat_dialog_active == 'yes'){
+
+                $.ajax({
+                    url:"group_chat.php",
+                    method:"POST",
+                    data:{action:action},
+                    success:function(data){
+
+                        $('#group_chat_history').html(data);
+
+                    }
+                });
+
+            }
+
+        }
         
     });
 </script>
