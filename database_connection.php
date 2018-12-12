7d39e76ab94b048f82205828401b88913c769164
Paweł Urbańczyk <pawel.urbanczyk90@gmail.com>
2018-12-12 12:26:54 +0100

    Dodanie opcji usuwania wysłanej wiadomosci w czacie grupowym.

--- database_connection.php
+++ database_connection.php
@@ -135,20 +135,35 @@ function fetch_group_chat_history($connect){
     foreach ($result as $row){
 
         $user_name = '';
+        $dynamic_background = '';
+        $chat_message = '';
         if($row['from_user_id'] == $_SESSION['user_id']){
-
-            $user_name = '<b class="text-success">You</b>';
+            if($row["status"] == '2'){
+                $chat_message = '<em>This message has been removed</em>';
+                $user_name = '<b class="text-success">You</b>';
+            }else{
+                $chat_message = $row["chat_message"];
+                $user_name = '<button type="button" class="btn btn-danger btn-xs remove_chat" id="'.$row['chat_message_id'].'">x</button>&nbsp;<b class="text-success">You</b>';
+            }
+            $dynamic_background = 'background-color:#ffe6e6;';
 
         }else{
+            if($row["status"] == '2'){
+                $chat_message = '<em>This message has been removed</em>';
 
+            }else{
+                $chat_message = $row["chat_message"];
+
+            }
             $user_name = '<b class="text-danger">'.get_user_name($row['from_user_id'], $connect).'</b>';
+            $dynamic_background = 'background-color:#ffffe6;';
 
         }
 
         $output .= '
 
-                    <li style="border-bottom:1px dotted #ccc">
-                        <p>'.$user_name.' - '.$row['chat_message'].'
+                    <li style="border-bottom:1px dotted #ccc;padding-top: 8px;padding-left: 8px;padding-right: 8px;'.$dynamic_background.'">
+                        <p>'.$user_name.' - '.$chat_message.'
                             <div align="right">
                                 - <small><em>'.$row['timestamp'].'</em></small>
                             </div>
